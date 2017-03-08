be14b2cc66e917f917fcb7b251c2c3e8529d4e7d
zhouwei <170491295@qq.com>
2017-03-08 18:25:02 +0800

    基于java配置的springMvc最小化配置

---
+++ platform-server/src/main/java/com/zhouwei/platform/config/WebInitializer.java
@@ -0,0 +1,23 @@
+package com.zhouwei.platform.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
+/**
+ * Created by zhouwei on 2017/3/8.
+ */
+public class WebInitializer implements WebApplicationInitializer {
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+        ctx.setServletContext(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ platform-server/src/main/java/com/zhouwei/platform/controller/hello/HelloController.java
@@ -0,0 +1,37 @@
+package com.zhouwei.platform.controller.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by zhouwei on 2017/3/3.
+ */
+@Controller
+public class HelloController {
+
+//    @Autowired
+//    AppInfo appInfo;
+
+//    @Autowired
+//    DataSource dataSource;
+
+    @RequestMapping("/hello")
+    public @ResponseBody String test() {
+        return "hello, world! This com from spring!";
+    }
+
+//    @RequestMapping("/appinfo")
+//    public @ResponseBody String appinfo(){
+//        return appInfo.toString();
+//    }
+
+//    @RequestMapping("/dbinfo")
+//    public @ResponseBody String dbinfo() throws SQLException {
+//        return dataSource.getConnection("root", "root123").getCatalog();
+//    }
+
+}
